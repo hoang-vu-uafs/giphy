1b28e9c40796b380b8b807df307315f6d9c37ad4
Hoang Vu <hoang.vu@uafs.edu>
2020-07-21 18:18:31 -0500

    Updates

---
+++ precache-manifest.bf6c27076af6e3022bcd7415cde618d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1bbf9e5f3cbd46455e8f9611232218",
+    "url": "/giphy/index.html"
+  },
+  {
+    "revision": "dc0453a8f17d1260a9c1",
+    "url": "/giphy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4f17eece7aa1bfa0c415",
+    "url": "/giphy/static/js/2.6591e1a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/giphy/static/js/2.6591e1a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc0453a8f17d1260a9c1",
+    "url": "/giphy/static/js/main.b05240b1.chunk.js"
+  },
+  {
+    "revision": "31d186ba0639c7ec143d",
+    "url": "/giphy/static/js/runtime-main.302485ce.js"
+  }
+]);
